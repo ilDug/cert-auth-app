18d678776d5953ff53c44d19632e4ec16feb36c7
ilDug <dognini.marco@gmail.com>
2022-12-11 14:21:57 +0100

    creating server

--- api/config/conf.py
+++ api/config/conf.py
@@ -4,6 +4,23 @@ import uuid
 
 MODE = config("MODE")
 
+IMPORT_PATH = Path("/import")
+PKI_PATH = Path("/PKI")
+KEYS_PATH = PKI_PATH / "private"
+REQS_PATH = PKI_PATH / "reqs"
+CONFIGS_PATH = PKI_PATH / "configs"
+CONFIG_FILE = CONFIGS_PATH / "openssl.cnf"
+CERTS_PATH = PKI_PATH / "certs"
+PASSPRHASE_PATH = PKI_PATH / "private/_passphrase"
+PUBLIC_PATH = PKI_PATH / "public"
+
+CA_CRT_PATH = CERTS_PATH / "ca.crt"
+CA_KEY_PATH = KEYS_PATH / "ca.key"
+CA_PUB_PATH = PUBLIC_PATH / "ca.pub.pem"
+
+CNF_BASE_DEFAULT = Path(__file__).parents[0] / "config/default.cnf"
+CNF_BASE_OPENSSL = Path(__file__).parents[0] / "config/openssl.cnf"
+
 # IMAGES AND MEDIA
 ###############################
 # ASSETS_PATH = Path(__file__).parents[1] / "assets"
