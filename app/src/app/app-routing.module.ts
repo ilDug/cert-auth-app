dfe49228dc9d679930e3b6d65a96ce12ddd7a62c
ilDug <dognini.marco@gmail.com>
2022-12-15 22:02:21 +0100

    navigation

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CertificatesComponent } from './certificates/certificates.component';
+import { KeysComponent } from './keys/keys.component';
+import { SettingsComponent } from './settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: "", pathMatch: "full", redirectTo: "certificates" },
+    { path: "certificates", component: CertificatesComponent },
+    { path: "keys", component: KeysComponent },
+    { path: "settings", component: SettingsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
