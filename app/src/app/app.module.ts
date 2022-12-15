47e28e02cd3293ddab6421d29e913eb7c8621c0b
ilDug <dognini.marco@gmail.com>
2022-12-15 22:02:21 +0100

    navigation

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -6,10 +6,16 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { RouterModule } from '@angular/router';
+import { CertificatesComponent } from './certificates/certificates.component';
+import { SettingsComponent } from './settings/settings.component';
+import { KeysComponent } from './keys/keys.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CertificatesComponent,
+    SettingsComponent,
+    KeysComponent
   ],
   imports: [
     BrowserModule,
