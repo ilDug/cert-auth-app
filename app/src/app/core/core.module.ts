62e4ac0c1e685014d138bae30d5e9bfdb54d4ccc
ilDug <dognini.marco@gmail.com>
2022-12-17 11:46:06 +0100

    tabs

--- app/src/app/core/core.module.ts
+++ app/src/app/core/core.module.ts
@@ -6,7 +6,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { MatTabsModule } from "@angular/material/tabs"
+import { ClipboardModule } from '@angular/cdk/clipboard';
+import { MatTooltipModule } from '@angular/material/tooltip'
 
 @NgModule({
     declarations: [],
@@ -21,7 +23,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
         FormsModule,
         ReactiveFormsModule,
         MatCheckboxModule,
-
+        MatTabsModule,
+        ClipboardModule,
+        MatTooltipModule,
     ]
 })
 export class CoreModule { }
