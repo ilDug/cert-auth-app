ba9c6114cb0e1f9522430db60ff1281ab39eefb2
ilDug <dognini.marco@gmail.com>
2022-12-17 14:48:51 +0100

    add certificate

--- app/src/app/certificates/add-certificate/add-certificate.component.ts
+++ app/src/app/certificates/add-certificate/add-certificate.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { CertificateSigningRequest } from 'src/app/core/classes/certificate.class';
 
 @Component({
-  selector: 'app-add-certificate',
-  templateUrl: './add-certificate.component.html',
-  styleUrls: ['./add-certificate.component.scss']
+    selector: 'app-add-certificate',
+    templateUrl: './add-certificate.component.html',
+    styleUrls: ['./add-certificate.component.scss']
 })
-export class AddCertificateComponent {
+export class AddCertificateComponent implements OnInit {
+
+    constructor(
+        private dialogRef: MatDialogRef<AddCertificateComponent>
+    ) { }
+
+    form: FormGroup
+
+    ngOnInit(): void {
+        const controls = {
+            subject: new FormControl(null),
+            wildcard: new FormControl(true)
+        }
+
+        this.form = new FormGroup(controls)
+    }
+
+
+    save() {
+        if (this.form.invalid) return;
+
+        let { subject, wildcard } = this.form.value
+        let csr = new CertificateSigningRequest({ subject: subject })
+        if (wildcard)
+            csr.alt_names.push(`*.${subject}`)
+
+        this.dialogRef.close(csr)
+    }
 
 }
