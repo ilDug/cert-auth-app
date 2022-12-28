5ed8238d9329b6b1195ae041639bf4a35618f236
ilDug <dognini.marco@gmail.com>
2022-12-28 22:01:12 +0100

    import zip [API]

--- api/controllers/importer.py
+++ api/controllers/importer.py
@@ -1,44 +1,44 @@
-# import os
-# from pathlib import Path
-# import subprocess
+from pathlib import Path
+from .installer import Installer
+import zipfile
+from config.conf import PKI_PATH
+from fastapi import HTTPException
 
-# from .installer import Installer
-# import typer
-# from core.settings import CA_KEY_PATH, PASSPRHASE_PATH, CA_CRT_PATH, IMPORT_PATH
+class Importer(Installer):
+    def __init__(self) -> None:
+        super().__init__()
 
+    def extract_pki(self, archive: Path):
+        try:
+            with zipfile.ZipFile(archive, mode="r") as zip_file:
+                for file in zip_file.namelist():
+                    zip_file.extract(file, PKI_PATH)
+            return True
+        except Exception:
+            raise HTTPException(500, "impossibile estrare archivio")
 
-# class Importer(Installer):
-#     def __init__(self) -> None:
-#         super().__init__()
+    # def import_ca(self):
+    #     crt = self.import_crt()
+    #     key = self.import_key()
 
-#         # controlla che esista la cartella di importaizone e che contanga i file
-#         if not IMPORT_PATH.exists() or not any(IMPORT_PATH.iterdir()):
-#             raise typer.Exit(
-#                 "la cartella di importazione '/import' non contiene i file necessari,  caricare nella cartelal il certificato (.crt) e la chiave privata (.key)"
-#             )
+    #     if crt and key:
+    #         CA_KEY_PATH.write_text(key.read_text())
+    #         CA_CRT_PATH.write_text(crt.read_text())
+    #         self.register_passphrase()
+    #     else:
+    #         typer.Exit("non sono stati trovati i file necessari all'importazione")
 
-#     def import_ca(self):
-#         crt = self.import_crt()
-#         key = self.import_key()
+    # def import_crt(self) -> Path:
+    #     for file in IMPORT_PATH.iterdir():
+    #         if file.is_file() and file.suffix == ".crt":
+    #             return file
 
-#         if crt and key:
-#             CA_KEY_PATH.write_text(key.read_text())
-#             CA_CRT_PATH.write_text(crt.read_text())
-#             self.register_passphrase()
-#         else:
-#             typer.Exit("non sono stati trovati i file necessari all'importazione")
+    # def import_key(self) -> Path:
+    #     for file in IMPORT_PATH.iterdir():
+    #         if file.is_file() and file.suffix == ".key":
+    #             return file
 
-#     def import_crt(self) -> Path:
-#         for file in IMPORT_PATH.iterdir():
-#             if file.is_file() and file.suffix == ".crt":
-#                 return file
-
-#     def import_key(self) -> Path:
-#         for file in IMPORT_PATH.iterdir():
-#             if file.is_file() and file.suffix == ".key":
-#                 return file
-
-#     def register_passphrase(self):
-#         passphrase = typer.prompt("inserire la passphrase")
-#         PASSPRHASE_PATH.write_text(passphrase)
-#         PASSPRHASE_PATH.chmod(400)
+    # def register_passphrase(self):
+    #     passphrase = typer.prompt("inserire la passphrase")
+    #     PASSPRHASE_PATH.write_text(passphrase)
+    #     PASSPRHASE_PATH.chmod(400)
--- api/routers/pki_utils.py
+++ api/routers/pki_utils.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Body, BackgroundTasks
+from pathlib import Path
+from fastapi import APIRouter, Body, BackgroundTasks, UploadFile, HTTPException
 from fastapi.responses import PlainTextResponse, FileResponse
-from controllers import Installer, PKIController
+from controllers import Installer, PKIController, Importer
 from core.install import install
-
+import aiofiles
 
 router = APIRouter(tags=["import"])
 
@@ -25,3 +26,21 @@ async def export_pki(background_tasks: BackgroundTasks):
     return FileResponse(pathfile, media_type="application/zip", filename=filename)
 
     # """genera tutta l'infrastruttira della PKI importando la chiave ed il certificato root. Eventuali strutture esistenti verranno cancellate"""
+
+
+@router.post("/pki/import/archive")
+async def import_pki_zip(archive: UploadFile):
+    if archive.content_type != "application/zip":
+        raise HTTPException(400, "il file deve essere un archivio zip")
+
+    importer = Importer()
+    importer.clean_structure()
+
+    temp_path = Path("/tmp/imported_pki.zip")
+    async with aiofiles.open(temp_path, "wb") as out_file:
+        while content := await archive.read(1024):  # async read chunk
+            await out_file.write(content)  # async write chunk
+
+    return importer.extract_pki(temp_path)
+
+    
