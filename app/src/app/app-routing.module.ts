f1bf12f7c92168e36659708ea615b20f81f6df0c
ilDug <dognini.marco@gmail.com>
2022-12-30 20:38:08 +0100

    [APP] export PKI

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -22,7 +22,7 @@ const routes: Routes = [
             { path: ":key", component: KeysDisplayComponent }
         ]
     },
-    { path: "settings", component: SettingsComponent }
+  { path: "settings", component: SettingsComponent }
 ];
 
 @NgModule({
--- app/src/app/settings/settings.component.ts
+++ app/src/app/settings/settings.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgxToastService } from '@ildug/ngx-toast';
+import { PKIService } from '../pki.service';
 
 @Component({
   selector: 'app-settings',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent {
+    constructor(
+        public pki$: PKIService,
+        private toast: NgxToastService
+    ) { }
 
+
+    downloadPki() {
+        this.pki$.exportPki().subscribe(filename => this.toast.info(`PKI con successo ${filename}`, 3000))
+    }
 }
