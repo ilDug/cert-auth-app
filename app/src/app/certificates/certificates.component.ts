467f1a16324cd7ff9045ace6629afac05451cfd5
ilDug <dognini.marco@gmail.com>
2024-06-05 22:24:04 +0200

    chore: Update certificates component HTML and TypeScript files

--- app/src/app/certificates/certificates.component.ts
+++ app/src/app/certificates/certificates.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { CertificatesService } from '../core';
 
 @Component({
   selector: 'ca-certificates',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './certificates.component.html',
   styles: ``
 })
 export class CertificatesComponent {
+    private dialog = inject(MatDialog);
+    private certs$ = inject(CertificatesService);
 
+    addCertificate(){
+
+    }
 }
