07e39a5e6e01a6f91a03dace9f5faf9155622109
ilDug <dognini.marco@gmail.com>
2024-06-23 16:58:53 +0200

    refactor: Update UploadListDirective to limit the number of files and add file removal functionality
    - Add filesLimit input to specify the maximum number of files allowed in the list
    - Throw an error if the maximum number of files is reached when adding a new file
    - Implement clear() method to remove all files from the list
    - Implement remove() method to remove a specific file from the list

--- app/src/app/core/upload/upload-list.directive.ts
+++ app/src/app/core/upload/upload-list.directive.ts
@@ -1,7 +1,7 @@
 import { Directive, computed, effect, inject, input, output, signal, viewChildren } from '@angular/core';
 import { UploadService } from './upload.service';
 import { UploadItemDirective } from './upload-item.directive';
-import { Observable, merge, of } from 'rxjs';
+import { Observable, Subject, merge, of } from 'rxjs';
 
 @Directive({
     selector: '[UploadList]',
@@ -10,40 +10,54 @@ import { Observable, merge, of } from 'rxjs';
 })
 export class UploadListDirective {
 
-    upload$ = inject(UploadService)
 
-    uploaded = output<any>();
+    upload$ = inject(UploadService)
+    upload = new Subject();
 
     files = signal<File[]>([]);
     length = computed(() => this.files().length);
     empty = computed(() => this.length() === 0);
 
+    // numero massimo di file ammessi 
+    filesLimit = input<number>(1); 
+
     endpoint = input.required<string>();
     #endpointEffect = effect(() => this.upload$.endpoint = this.endpoint());
 
-    items = viewChildren(UploadItemDirective);
-
-    requests = computed<Observable<string>[]>(() => {
-        console.log('files', this.files());
-        return this.items()
-            .filter(item => !item.completed)
-            .map(item => this.upload$.upload(item));
-    });
-
+    // aggiiunge un file alla lista
     add(file: File) {
+        if (this.length() >= this.filesLimit()) {
+            throw new Error(`Numero massimo di file raggiunto. MAX: ${this.filesLimit()}`);
+        }
         this.files.update(files => [...files, file]);
     }
 
+    // elimina tutti i file dalla lista
+    clear() {
+        this.files.set([]);
+    }
+
+    // rimuove un file dalla lista
     remove(file: File) {
         this.files.update(files => files.filter(f => f !== file));
     }
 
-    upload() {
-        /** emette uno alla volta */
-        const s = merge(...this.requests())
-            .subscribe(results => this.uploaded.emit(results));
-
-        /** emette alla  fine */
-        // const s = forkJoin(uploadRequests).subscribe(results => this.uploadEvent.emit(results));
-    }
 }
+
+
+// items = viewChildren(UploadItemDirective);
+
+// requests = computed<Observable<string>[]>(() => {
+//     console.log('files', this.files());
+//     return this.items()
+//         .filter(item => !item.completed)
+//         .map(item => this.upload$.upload(item));
+// });
+
+
+/** emette uno alla volta */
+// const s = merge(...this.requests())
+//     .subscribe(results => this.uploaded.emit(results));
+
+/** emette alla  fine */
+// const s = forkJoin(uploadRequests).subscribe(results => this.uploadEvent.emit(results));
