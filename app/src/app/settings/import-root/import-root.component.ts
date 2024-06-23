4c533250f9d598aa1beff7058c7031807747b951
ilDug <dognini.marco@gmail.com>
2024-06-23 16:49:01 +0200

    addinge empty setting module

---
+++ app/src/app/settings/import-root/import-root.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ca-import-root',
+  standalone: true,
+  imports: [],
+  templateUrl: './import-root.component.html',
+  styles: ``
+})
+export class ImportRootComponent {
+
+}
