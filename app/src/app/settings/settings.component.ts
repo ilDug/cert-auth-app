7a0c92acb713bb22e2703974385b5ceba9bcf2a0
ilDug <dognini.marco@gmail.com>
2024-06-23 16:49:30 +0200

    adding child componet to setting page

--- app/src/app/settings/settings.component.ts
+++ app/src/app/settings/settings.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ImportPkiComponent } from './import-pki/import-pki.component';
+import { ImportRootComponent } from './import-root/import-root.component';
 
 @Component({
   selector: 'ca-settings',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, ImportPkiComponent, ImportRootComponent],
   templateUrl: './settings.component.html',
   styles: ``
 })
