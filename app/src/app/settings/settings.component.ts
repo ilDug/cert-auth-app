c2d227daecbd91df8ecb5aa943c451a4ebeaca4f
ilDug <dognini.marco@gmail.com>
2024-06-25 22:04:37 +0200

    import root module

--- app/src/app/settings/settings.component.ts
+++ app/src/app/settings/settings.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ImportPkiComponent } from './import-pki/import-pki.component';
 import { ImportRootComponent } from './import-root/import-root.component';
+import { NgxToastModule, NgxToastService } from '@ildug/ngx-toast';
+import { NgxConfirmDirective } from '@ildug/ngx-confirm';
 
 @Component({
   selector: 'ca-settings',
   standalone: true,
-  imports: [RouterModule, ImportPkiComponent, ImportRootComponent],
+    imports: [RouterModule, ImportPkiComponent, ImportRootComponent, NgxToastModule, NgxConfirmDirective],
   templateUrl: './settings.component.html',
   styles: ``
 })
 export class SettingsComponent {
+    toast = inject(NgxToastService) 
 
+    alert() {
+        this.toast.info('Questo è un messaggio di alert')
+    }
 }
