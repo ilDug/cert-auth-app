9bd2d36c6273f282898d313b41197380f1301dda
ilDug <dognini.marco@gmail.com>
2024-07-02 18:41:33 +0200

    Models

---
+++ api/core/models/certificate.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel
+
+
+class CommonNameBodyRequest(BaseModel):
+    subject: str
+    alt_names: list[str] = []
+    days: int = 1825
+
+
+class Certificate(BaseModel):
+    status: str
+    exp_date: datetime
+    revoke_date: datetime | None = None
+    serial: int
+    filename_unknown: str
+    common_name: str
+
+
+class RootPackage(BaseModel):
+    crt: str
+    key: str
+    passphrase: str
